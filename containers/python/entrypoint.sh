f3d2d10c97c43d729afada7260e4fe8f0ce34ea3
taku ikegami <takuron1996@gmail.com>
2023-04-06 08:59:17 +0900

    DEBUG時にホットリロードを有効化

--- containers/python/entrypoint.sh
+++ containers/python/entrypoint.sh
@@ -4,4 +4,10 @@ set -eu
 
 poetry run python manage.py makemigrations
 poetry run python manage.py migrate
-poetry run gunicorn application.wsgi:application --bind 0.0.0.0:8000
+
+if [ $DEBUG = "True" ]
+then
+    poetry run python manage.py runserver 0.0.0.0:8000
+else
+    poetry run gunicorn application.wsgi:application --bind 0.0.0.0:8000
+fi
